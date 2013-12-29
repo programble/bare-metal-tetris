e02c9879a487cc713529a945e81d2b50b3f94710
Curtis McEnroe <programble@gmail.com>
2013-12-29 15:31:28 -0500

    Timing, video output, keyboard input

---
+++ Makefile
@@ -0,0 +1,54 @@
+CC = gcc
+ASM = nasm
+LD = ld
+
+CFLAGS = -Wall -Wextra -Wunreachable-code -Wcast-qual -Wcast-align -Wswitch-enum -Wmissing-noreturn -Wwrite-strings -Wundef -Wpacked -Wredundant-decls -Winline -Wdisabled-optimization -m32 -nostdinc -ffreestanding -fno-builtin
+AFLAGS = -f elf
+LFLAGS = -melf_i386 -nostdlib -T linker.ld
+
+tetris.elf: entry.o tetris.o
+	$(LD) $(LFLAGS) $^ -o $@
+
+entry.o: entry.asm
+	$(ASM) $(AFLAGS) $< -o $@
+
+tetris.o: tetris.c
+	$(CC) $(CFLAGS) $< -c -o $@
+
+GENISOIMAGE = genisoimage
+ISOHYBRID = isohybrid
+ISOLINUXBIN = /usr/lib/syslinux/isolinux.bin
+ISOLINUXMBOOTC32 = /usr/lib/syslinux/mboot.c32
+
+tetris.iso: iso/boot/tetris.elf iso/boot/isolinux/isolinux.bin iso/boot/isolinux/mboot.c32 iso/boot/isolinux/isolinux.cfg
+	$(GENISOIMAGE) -R -b boot/isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -o $@ iso
+	$(ISOHYBRID) $@
+
+iso/boot/tetris.elf: tetris.elf
+	@mkdir -p iso/boot
+	cp $< $@
+
+iso/boot/isolinux/isolinux.bin: $(ISOLINUXBIN)
+	@mkdir -p iso/boot/isolinux
+	cp $< $@
+
+iso/boot/isolinux/mboot.c32: $(ISOLINUXMBOOTC32)
+	@mkdir -p iso/boot/isolinux
+	cp $< $@
+
+iso/boot/isolinux/isolinux.cfg: isolinux.cfg
+	@mkdir -p iso/boot/isolinux
+	cp $< $@
+
+QEMU = qemu-system-i386
+
+qemu: tetris.elf
+	$(QEMU) -kernel $<
+
+qemu-iso: tetris.iso
+	$(QEMU) -cdrom $<
+
+clean:
+	rm -rf tetris.elf entry.o tetris.o iso
+
+.PHONY: qemu qemu-iso clean
---
+++ tetris.c
@@ -0,0 +1,191 @@
+typedef unsigned char      u8;
+typedef signed   char      s8;
+typedef unsigned short     u16;
+typedef signed   short     s16;
+typedef unsigned int       u32;
+typedef signed   int       s32;
+typedef unsigned long long u64;
+typedef signed   long long s64;
+
+#define noreturn __attribute__((noreturn)) void
+
+typedef enum bool {
+    false,
+    true
+} bool;
+
+/* Port I/O */
+
+static inline u8 inb(u16 p)
+{
+    u8 r;
+    asm("inb %1, %0" : "=a" (r) : "dN" (p));
+    return r;
+}
+
+static inline void outb(u16 p, u8 d)
+{
+    asm("outb %1, %0" : : "dN" (p), "a" (d));
+}
+
+/* Timing */
+
+static inline u64 rdtsc(void)
+{
+    u32 hi, lo;
+    asm("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((u64) lo) | (((u64) hi) << 32);
+}
+
+#define RTC_OUT (0x70)
+#define RTC_IN  (0x71)
+
+u8 rtcs(void)
+{
+    u8 last = 0, secs;
+    do { /* until value is the same twice in a row */
+        /* wait for update not in progress */
+        do { outb(RTC_OUT, 0x0A); } while (inb(RTC_IN) & 0x80);
+        outb(RTC_OUT, 0x00);
+        secs = inb(RTC_IN);
+    } while (secs != last && (last = secs));
+    return secs;
+}
+
+u64 tps(void)
+{
+    static u64 ti = 0, dt = 0;
+    static u8 last_secs = 0xFF;
+    u8 secs = rtcs();
+    if (secs != last_secs) {
+        last_secs = secs;
+        u64 tf = rdtsc();
+        dt = tf - ti;
+        ti = tf;
+    }
+    return dt;
+}
+
+enum timer {
+    TIMER_BLINK,
+    TIMER__LENGTH
+};
+
+u64 timers[TIMER__LENGTH] = {0};
+
+bool interval(enum timer timer, u64 ticks)
+{
+    u64 tf = rdtsc();
+    u64 dt = tf - timers[timer];
+    if (dt >= ticks) {
+        timers[timer] = tf;
+        return true;
+    } else return false;
+}
+
+/* Video Output */
+
+enum color {
+    BLACK,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    MAGENTA,
+    YELLOW,
+    GRAY,
+    BRIGHT
+};
+
+#define COLS (80)
+#define ROWS (25)
+u16 *const video = (u16*) 0xB8000;
+
+void putc(u8 x, u8 y, enum color fg, enum color bg, char c)
+{
+    u16 z = (bg << 12) | (fg << 8) | c;
+    video[y * COLS + x] = z;
+}
+
+void puts(u8 x, u8 y, enum color fg, enum color bg, const char *s)
+{
+    for (; *s; s++, x++)
+        putc(x, y, fg, bg, *s);
+}
+
+void clear(enum color bg)
+{
+    u8 x, y;
+    for (y = 0; y < ROWS; y++)
+        for (x = 0; x < COLS; x++)
+            putc(x, y, bg, bg, ' ');
+}
+
+/* Keyboard Input */
+
+#define KEY_UP    (0x48)
+#define KEY_DOWN  (0x50)
+#define KEY_LEFT  (0x4B)
+#define KEY_RIGHT (0x4D)
+#define KEY_ENTER (0x1C)
+#define KEY_SPACE (0x39)
+
+u8 scan(void)
+{
+    static u8 key = 0;
+    u8 scan = inb(0x60);
+    if (scan != key)
+        return key = scan;
+    else return 0;
+}
+
+/* Formatting */
+
+char *itoa(u32 n, u8 r, u8 w)
+{
+    static const char d[16] = "0123456789ABCDEF";
+    static char s[34];
+    s[33] = 0;
+    u8 i = 33;
+    do {
+        i--;
+        s[i] = d[n % r];
+        n /= r;
+    } while (i > 33 - w);
+    return (char *) (s + i);
+}
+
+noreturn main()
+{
+    clear(BLACK);
+    puts(0, 0, BRIGHT | GREEN, BLACK, "Hello, World!");
+    puts(0, 1, GREEN, BLACK, itoa((u32) &main, 16, 8));
+
+    u32 tpms;
+    bool blink = false;
+    u8 key, x = 40, y = 13;
+loop:
+    tpms = (u32) tps() / 1000;
+
+    puts(0, 2, YELLOW, BLACK, itoa(rtcs(), 16, 2));
+    puts(3, 2, YELLOW, BLACK, itoa(tpms, 10, 10));
+
+    if (interval(TIMER_BLINK, tpms * 500))
+        blink = !blink;
+    puts(0, 3, blink ? MAGENTA : BLACK, blink ? BLACK : MAGENTA, "blink");
+
+    if ((key = scan())) {
+        puts(0, 4, RED, BLACK, itoa(key, 16, 2));
+
+        putc(x, y, BLACK, BLACK, '@');
+        switch (key) {
+        case KEY_UP:    y--; break;
+        case KEY_DOWN:  y++; break;
+        case KEY_LEFT:  x--; break;
+        case KEY_RIGHT: x++; break;
+        }
+        putc(x, y, BRIGHT | YELLOW, YELLOW, '@');
+    }
+
+    goto loop;
+}
